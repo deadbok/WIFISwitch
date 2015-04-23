f21be49e64c2b85f8b839f36007e5608eaff01bb
deadbok <martin.groenholdt@gmail.com>
2015-04-24 00:12:05 +0200

    Hello world http server.

--- user/tcp.c
+++ user/tcp.c
@@ -24,24 +24,101 @@
 #include "espconn.h"
 #include "user_config.h"
 
-static void ICACHE_FLASH_ATTR tcp_connect_cb(void *arg)
+//Active connection.
+static struct espconn tcp_conn;
+static esp_tcp tcp;
+
+//Internal function to print espconn errors.
+static void print_status(int status)
 {
-    debug("TCP connection.\n");
+    switch(status)
+    {
+        case ESPCONN_OK:
+            debug("OK.\n");
+            break;
+        case ESPCONN_MEM:
+            os_printf("Out of memory.\n");
+            break;
+        case ESPCONN_TIMEOUT:
+            os_printf("Timeout.\n");
+            break;            
+        case ESPCONN_RTE:
+            os_printf("Routing problem.\n");
+            break;
+        case ESPCONN_INPROGRESS:
+            os_printf("Operation in progress...\n");
+            break;
+        case ESPCONN_ABRT:
+            os_printf("Connection aborted.\n");
+            break;
+        case ESPCONN_RST:
+            os_printf("Connection reset.\n");
+            break;
+        case ESPCONN_CLSD:
+            os_printf("Connection closed.\n");
+            break;
+        case ESPCONN_CONN:
+            os_printf("Not connected.\n");
+            break;
+        case ESPCONN_ARG:
+            os_printf("Illegal argument.\n");
+            break;
+        case ESPCONN_ISCONN:
+            os_printf("Already connected.\n");
+            break;
+        default:
+            os_printf("Unknown status: %d\n", status);
+            break;    
+    }
+}
+
+static void ICACHE_FLASH_ATTR tcp_recv_cb(void *arg, char *data, unsigned short length)
+{
+    char    *response = "HTTP/1.0 200 OK\r\nContent-Type: text/html\
+                         \r\nContent-Length: ";
+    char    *html = "<!DOCTYPE html><head><title>Web server test.</title></head>\
+                     <body>Hello world.</body></html>";
+    char    html_length[6];
+    
+    debug("TCP recieved:\n");
+    debug("%s\n", data);
+
+    if(os_strncmp(data,"GET ",4) && os_strncmp(data,"get ",4))
+    {
+        os_printf("Unknown request.\n");
+    }
+    else
+    {
+        debug("GET request.\n");
+        espconn_sent(&tcp_conn, (unsigned char *)response, os_strlen(response));
+        os_sprintf(html_length, "%d", os_strlen(html));
+        espconn_sent(&tcp_conn, (unsigned char *)html_length, os_strlen(html_length));
+        espconn_sent(&tcp_conn, (unsigned char *)"\r\n\r\n", 4);
+        espconn_sent(&tcp_conn, (unsigned char *)html, os_strlen(html));
+    }
 }
 
 void ICACHE_FLASH_ATTR init_tcp(int port)
 {
-    static struct espconn tcp_conn;
-    static esp_tcp tcp;
+    int ret;
     
     debug("TCP init.\n");
     
-
-    tcp_conn.type = ESPCONN_TCP;
-    tcp_conn.state = ESPCONN_NONE;
+    //TCP Setup
+    //Port to bind to.
     tcp.local_port = port;
-    tcp_conn.proto.tcp = &tcp;
     
-    espconn_regist_connectcb(&tcp_conn, tcp_connect_cb);
-    espconn_accept(&tcp_conn);  
+    //Setup TCP connection configuration.
+    //TCP connection
+    tcp_conn.type = ESPCONN_TCP;
+    //No state.
+    tcp_conn.state = ESPCONN_NONE;
+    //TCP setup
+    tcp_conn.proto.tcp = &tcp;
+    //
+    tcp_conn.recv_callback = tcp_recv_cb;
+    
+    debug("Accepting connections on port %d...", port);
+    ret = espconn_accept(&tcp_conn);
+    print_status(ret); 
 }
