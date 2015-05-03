5b6a0f0083a4efc7a89a427a19e69cf23c2f84a1
deadbok <martin.groenholdt@gmail.com>
2015-05-03 23:27:41 +0200

    Clearer error reporting.

--- user/wifi_connect.c
+++ user/wifi_connect.c
@@ -63,13 +63,13 @@ static unsigned char ICACHE_FLASH_ATTR wifi_disconnect(void)
         ret = wifi_station_disconnect();
         if (!ret)
         {
-            os_printf("Cannot disconnect (%d).", ret);
+            os_printf("ERROR: Cannot disconnect (%d).", ret);
             return(ret);
         }
         ret = wifi_station_dhcpc_stop();
         if (!ret)
         {
-            os_printf("Cannot stop DHCP client (%d).", ret);
+            os_printf("ERROR: Cannot stop DHCP client (%d).", ret);
             return(255);
         }    
     }
@@ -123,7 +123,7 @@ static void connect(void *arg)
             ret = wifi_get_ip_info(STATION_IF, &ipinfo);
             if (!ret)
             {
-                os_printf("Failed get IP address (%d).\n", ret);
+                os_printf("ERROR: Failed get IP address (%d).\n", ret);
                 return;
             }
             os_printf("Got IP address: %d.%d.%d.%d.\n", IP2STR(&ipinfo.ip) );
@@ -143,7 +143,7 @@ static void connect(void *arg)
             }
             else
             {
-                os_printf("Failed to connect\n");
+                os_printf("ERROR: Failed to connect\n");
             }
             break;
         default:
@@ -183,7 +183,7 @@ static void ICACHE_FLASH_ATTR create_softap(char *ssid, char *passwd, unsigned c
     ret = wifi_set_opmode(SOFTAP_MODE);
     if (!ret)
     {
-        os_printf("Cannot set soft AP mode (%d).", ret);
+        os_printf("ERROR: Cannot set soft AP mode (%d).", ret);
         return;
     }
               
@@ -203,7 +203,7 @@ static void ICACHE_FLASH_ATTR create_softap(char *ssid, char *passwd, unsigned c
     ret = wifi_softap_set_config(&ap_config);
     if (!ret)
     {
-        os_printf("Failed to set soft AP configuration (%d).", ret);
+        os_printf("ERROR: Failed to set soft AP configuration (%d).", ret);
         return;
     }
 }
@@ -225,7 +225,7 @@ void timeout_cb(void)
     ret = wifi_get_macaddr(SOFTAP_IF, mac);
     if (!ret)
     {
-        os_printf("Cannot get MAC address (%d).", ret);
+        os_printf("ERROR: Cannot get MAC address (%d).", ret);
         return;
     }
     debug("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", MAC2STR(mac));
@@ -270,14 +270,14 @@ void ICACHE_FLASH_ATTR wifi_connect(void (*connect_cb)())
     ret = wifi_set_opmode(STATION_MODE);
     if (!ret)
     {
-        os_printf("Cannot set station mode (%d).", ret);
+        os_printf("ERROR: Cannot set station mode (%d).", ret);
         return;
     }
     //Get a pointer the configuration data.
     ret = wifi_station_get_config(&station_conf); 
     if (!ret)
     {
-        os_printf("Cannot get station configuration (%d).", ret);
+        os_printf("ERROR: Cannot get station configuration (%d).", ret);
         return;
     }
 
@@ -286,7 +286,7 @@ void ICACHE_FLASH_ATTR wifi_connect(void (*connect_cb)())
     ret = wifi_disconnect();
     if (!ret)
     {
-        os_printf("Cannot disconnect (%d).", ret);
+        os_printf("ERROR: Cannot disconnect (%d).", ret);
         return;
     }
     
@@ -294,7 +294,7 @@ void ICACHE_FLASH_ATTR wifi_connect(void (*connect_cb)())
     ret = wifi_station_set_config(&station_conf);
     if (!ret)
     {
-        os_printf("Failed to set station configuration (%d).\n", ret);
+        os_printf("ERROR: Failed to set station configuration (%d).\n", ret);
         return;
     }
     
