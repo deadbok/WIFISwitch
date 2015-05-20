251eabadd494e7d2dba602676ab5c30fbf2cb61e
deadbok <martin.groenholdt@gmail.com>
2015-05-20 19:41:32 +0200

    Oops, the user/tools folder was ignored.

---
+++ user/tools/itoa.h
@@ -0,0 +1,13 @@
+/*
+ * itoa.h
+ *
+ *  Created on: 17/09/2014
+ *      Author: oblivion
+ */
+
+#ifndef ITOA_H_
+#define ITOA_H_
+
+extern char *itoa(int value, char* result, int base);
+
+#endif /* ITOA_H_ */
---
+++ user/tools/missing_dec.h
@@ -0,0 +1,65 @@
+//I have not been able to find the original source of this file. I think
+//Martin Harizanov wrote it. I have modified it as I went along.
+//Martin Groenholdt
+
+//Missing function prototypes in include folders. Gcc will warn on these if we don't define 'em anywhere.
+//MOST OF THESE ARE GUESSED! but they seem to swork and shut up the compiler.
+
+#ifndef MISSING_DEC_H
+#define MISSING_DEC_H
+
+#include "ets_sys.h"
+#include "c_types.h"
+#include <stdarg.h>
+
+typedef s8 err_t;
+
+extern int atoi(const char *nptr);
+
+extern void ets_install_putc1(void *routine);
+
+extern void ets_isr_attach(int intr, void *handler, void *arg);
+extern void ets_isr_mask(unsigned intr);
+extern void ets_isr_unmask(unsigned intr);
+
+extern int ets_memcmp(const void *s1, const void *s2, size_t n);
+extern void *ets_memcpy(void *dest, const void *src, size_t n);
+extern void *ets_memset(void *s, int c, size_t n);
+
+extern int ets_sprintf(char *str, const char *format, ...)  __attribute__ ((format (printf, 2, 3)));
+extern int ets_vsprintf(char *str, const char *format, va_list argptr);
+extern int ets_vsnprintf(char *buffer, size_t sizeOfBuffer, const char *format, va_list argptr);
+
+extern int ets_str2macaddr(void *, void *);
+
+extern void ets_bzero(void *s, size_t n);
+
+extern int ets_strcmp(const char *s1, const char *s2);
+extern char *ets_strcpy(char *dest, const char *src);
+extern size_t ets_strlen(char *s);
+extern int ets_strncmp(const char *s1, const char *s2, int len);
+extern char *ets_strncpy(char *dest, const char *src, size_t n);
+extern char *ets_strstr(const char *haystack, const char *needle);
+
+extern void ets_timer_arm_new(ETSTimer *a, int b, int c, int isMstimer);
+extern void ets_timer_disarm(ETSTimer *a);
+extern void ets_timer_setfn(ETSTimer *t, ETSTimerFunc *fn, void *parg);
+
+extern int os_printf(const char *format, ...)  __attribute__ ((format (printf, 1, 2)));
+extern int os_snprintf(char *str, size_t size, const char *format, ...) __attribute__ ((format (printf, 3, 4)));
+extern int os_printf_plus(const char *format, ...)  __attribute__ ((format (printf, 1, 2)));
+
+extern void pvPortFree(void *ptr);
+extern void *pvPortMalloc(size_t xWantedSize);
+extern void *pvPortZalloc(size_t);
+
+extern void uart_div_modify(int no, unsigned int freq);
+
+extern void vPortFree(void *ptr);
+extern void *vPortMalloc(size_t xWantedSize);
+
+extern void ets_delay_us(int us);
+
+extern void slop_wdt_feed();
+
+#endif
