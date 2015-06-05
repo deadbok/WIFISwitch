358c6fc7a979720e5c861a059a8735c4154e0d01
deadbok <martin.groenholdt@gmail.com>
2015-06-05 13:26:41 +0200

    Forgot the changes to itoa.c.

--- user/tools/itoa.c
+++ user/tools/itoa.c
@@ -1,19 +1,43 @@
-/*
- * itoa.c
+/**
+ * @file itoa.c
  *
- * Unsigned integer to string conversion.
+ * @brief Integer to string conversion.
+ * 
  * Nicked from http://www.jb.man.ac.uk/~slowe/cpp/itoa.html.
  * Simplified for my use case, and did some cosmetic changes to make it easier to understand.
  */
- #include "os_type.h"
- 
-char ICACHE_FLASH_ATTR *itoa(unsigned int value, char* result, char base)
+#include "user_config.h" 
+#include "c_types.h"
+
+/**
+ * @brief Convert an integer to a string.
+ * 
+ * @param value The value to be converted.
+ * @param result A pointer to where the string is written.
+ * @param base The base used to convert the value.
+ * @return A pointer to #result, or NULL on error.
+ */ 
+char ICACHE_FLASH_ATTR *itoa(long value, char *result, const unsigned char base)
 {
-	char			*chr_map = "0123456789abcdef";
-	char			*ptr = result;
-	char			*ptr1 = result;
-	char			tmp_char;
-	unsigned int 	tmp_value;
+	const char *chr_map = "0123456789abcdef";
+	char *ptr = result;
+	char *ptr1 = result;
+	char tmp_char;
+	long tmp_value;
+	bool sign;
+	
+	if ((base == 0) || (base > 16))
+	{
+		error("Wrong base %d, when converting integer to string.\n", base);
+		return(NULL);
+	}
+	
+	//Negative number handling.
+	if ((value < 0) && (base == 10))
+	{
+		value = -value;
+		sign = true;
+	}
 
 	//Run until conversion is done
 	do {
@@ -27,11 +51,17 @@ char ICACHE_FLASH_ATTR *itoa(unsigned int value, char* result, char base)
         *ptr++ = chr_map[tmp_value - (value * base)];
 
 	} while (value);
-
+	//Add sign.
+	if (sign)
+	{
+		*ptr++ = '-';
+	}
+	//Add zero byte, and go back to the last real character.
 	*ptr-- = '\0';
+	
 	//Reverse the string
 	while (ptr1 < ptr) {
-		//Save character the right
+		//Save character to the right
 		tmp_char = *ptr;
 		//Replace the right character with the left
 		*ptr-- = *ptr1;
